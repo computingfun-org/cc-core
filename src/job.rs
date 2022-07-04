5c626d50ab3ee97f296bc0671b5cf2f46380bddf
Zachary Cohen <zachary@computingfun.org>
2022-07-03 21:28:44 -0400

    made JobNumber use derive_more macros

--- src/job.rs
+++ src/job.rs
@@ -1,32 +1,10 @@
-#[derive(Default, Copy, Clone, PartialEq, Eq)]
+use derive_more::{Display, From, FromStr, Into};
+
+#[derive(
+    Default, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, From, Into, FromStr, Display,
+)]
+#[display(fmt = "Job # {}", _0)]
 pub struct JobNumber(usize);
-
-impl JobNumber {
-    pub fn number(self) -> usize {
-        self.0
-    }
-
-    pub fn valid(self) -> bool {
-        self.0 != 0
-    }
-}
-
-impl<T: Into<usize>> From<T> for JobNumber {
-    fn from(job_number: T) -> Self {
-        JobNumber(job_number.into())
-    }
-}
-
-impl std::str::FromStr for JobNumber {
-    type Err = std::num::ParseIntError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.parse::<usize>() {
-            Ok(num) => Ok(JobNumber::from(num)),
-            Err(err) => Err(err),
-        }
-    }
-}
 
 #[derive(Clone, PartialEq, Eq)]
 pub struct JobURL(String);
@@ -45,7 +23,7 @@ impl<T: Into<JobNumber>> From<T> for JobURL {
     fn from(job_number: T) -> Self {
         JobURL(format!(
             "https://dashboard.calclosets.com/?j={}",
-            job_number.into().number()
+            usize::from(job_number.into())
         ))
     }
 }
